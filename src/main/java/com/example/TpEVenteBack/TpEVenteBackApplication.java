88f8a29de979f322472905ab80f195f2b0ed477c
Johan VAIRAC <johan.vairac@outlook.fr>
2019-06-10 14:08:17 +0200

    commit initial

---
+++ src/main/java/com/example/TpEVenteBack/TpEVenteBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.TpEVenteBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpEVenteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpEVenteBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TpEVenteBack/TpEVenteBackApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.TpEVenteBack;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TpEVenteBackApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
